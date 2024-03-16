5db5ac6104f5c7aba8565e29dc130f3bc46814cc
Filipe Jacobson Marra <filipejacobsonmarra@gmail.com>
2024-03-16 01:06:53 -0300

    Create Item.java

---
+++ PROGRAMAÇÃO ORIENTADA A OBJETOS (Java)/atividade1/Item.java
@@ -0,0 +1,39 @@
+class Item {
+    String tipo; // "produto" ou "servico"
+    double precoBase;
+    String descricao;
+    List<Imposto> impostos = new ArrayList<>();
+
+    public Item(String tipo, double precoBase, String descricao) {
+        this.tipo = tipo;
+        this.precoBase = precoBase;
+        this.descricao = descricao;
+        configurarImpostos();
+    }
+
+    private void configurarImpostos() {
+        if (tipo.equals("produto")) {
+            impostos.add(new ICMS());
+            impostos.add(new IPI());
+        } else if (tipo.equals("servico")) {
+            impostos.add(new ISS());
+            impostos.add(new ICMS()); // Supondo que serviços também pagam ICMS
+        }
+    }
+
+    public double calcularImpostoTotal() {
+        double totalImposto = 0;
+        for (Imposto imposto : impostos) {
+            totalImposto += imposto.calcular(precoBase);
+        }
+        return totalImposto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
+}
