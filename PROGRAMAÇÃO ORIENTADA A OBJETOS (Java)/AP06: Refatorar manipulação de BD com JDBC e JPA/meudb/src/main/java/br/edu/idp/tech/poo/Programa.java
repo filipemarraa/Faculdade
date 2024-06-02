c4f2bd505559617c181e05a57014af4187e69f0d
Filipe Jacobson Marra <filipejacobsonmarra@gmail.com>
2024-06-01 23:13:06 -0300

    Update Programa.java

--- PROGRAMAÇÃO ORIENTADA A OBJETOS (Java)/AP06: Refatorar manipulação de BD com JDBC e JPA/meudb/src/main/java/br/edu/idp/tech/poo/Programa.java
+++ PROGRAMAÇÃO ORIENTADA A OBJETOS (Java)/AP06: Refatorar manipulação de BD com JDBC e JPA/meudb/src/main/java/br/edu/idp/tech/poo/Programa.java
@@ -1,6 +1,6 @@
 package br.edu.idp.tech.poo;
 
-
+import br.edu.idp.tech.poo.dao.PersistenciaCarro;
 import br.edu.idp.tech.poo.dao.CarroJdbcDao;
 import br.edu.idp.tech.poo.dao.CarroJpaDao;
 import br.edu.idp.tech.poo.entidade.Carro;
@@ -9,7 +9,6 @@ import br.edu.idp.tech.poo.ui.CliIavel;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 
 public class Programa {
 
@@ -21,86 +20,29 @@ public class Programa {
     }
 
     public void executar() {
-        // passo 1 - criando a "marca" da execução
-
-        // marca criada para diferenciar o que é cadastrado em cada execução
         sufixo = " /" + gerarLetraAletoria();
         ui.exibirInformacao("Sufixo da execução: " + sufixo);
-
-        // passo 2 - criação do banco de dados
-
-        // criação da tabela
         prepararBD();
 
-        // TODO juntar os métodos executarJdbc e executarJpa em um só,
-        //   mas que permita a execução por JDBC ou por JPA.
-        //   o programa deve continuar execuntado uma "versão" por JDBC
-        //   (passo 3) e outra por JPA (passo 4).
+        ui.exibirInformacao("Execução via JDBC - início");
+        executarPersistencia(new CarroJdbcDao(), 3);
+        ui.exibirInformacao("Execução via JDBC - término");
 
-        // passo 3 - várias operações com JDBC
-
-        // versão 1: operações com JDBC
-        // - insere 3 carros
-        ui.exibirInformacao("execução via JDBC - início");
-        executarJdbc();
-        ui.exibirInformacao("execução via JDBC - término");
-
-        // passo 4 - várias operações com JPA
-
-        // versão 2: operações com JPA
-        // - insere 4 carros
-        ui.exibirInformacao("execução via JPA - início");
-        executarJpa();
-        ui.exibirInformacao("execução via JDBC - término");
-
-        // cada execução termina com 7 carros inseridos
-
+        ui.exibirInformacao("Execução via JPA - início");
+        executarPersistencia(new CarroJpaDao(), 4);
+        ui.exibirInformacao("Execução via JPA - término");
     }
 
-    private void executarJdbc() {
-        CarroJdbcDao dao = new CarroJdbcDao();
+    private void executarPersistencia(PersistenciaCarro dao, int quantidadeCarros) {
+        CarregadorDados carregador = new CarregadorDados(sufixo);
+        List<Carro> novosCarros = carregador.gerarListaCarros(quantidadeCarros);
 
-        criarRegistrosPorJdbc(dao);
+        for (Carro carro : novosCarros) {
+            dao.salvar(carro);
+        }
 
         List<Carro> carros = dao.findAll();
         ui.exibirListaCarros(carros);
-    }
-
-    private void executarJpa() {
-        CarroJpaDao dao = new CarroJpaDao();
-
-        criarRegistrosPorJpa(dao);
-
-        List<Carro> carros = dao.findAll();
-        ui.exibirListaCarros(carros);
-    }
-
-    private void criarRegistrosPorJdbc(CarroJdbcDao dao) {
-        // gerador aleatório de carros para "economizar" a inclusão de dados pelo usuário
-        CarregadorDados carregador = new CarregadorDados(sufixo);
-
-        Carro novoCarro = carregador.gerarCarro();
-        // salvar carro criado
-        dao.salvar(novoCarro);
-
-        List<Carro> novosCarros = new ArrayList<>();
-        novosCarros.addAll(carregador.gerarListaCarros(2));
-        // salvar coleção de carros
-        dao.salvar(novosCarros);
-    }
-
-    private void criarRegistrosPorJpa(CarroJpaDao dao) {
-        // gerador aleatório de carros para "economizar" a inclusão de dados pelo usuário
-        CarregadorDados carregador = new CarregadorDados(sufixo);
-
-        Carro novoCarro = carregador.gerarCarro();
-        // salvar carro criado
-        dao.salvar(novoCarro);
-
-        List<Carro> novosCarros = new ArrayList<>();
-        novosCarros.addAll(carregador.gerarListaCarros(3));
-        // salvar coleção de carros
-        dao.salvar(novosCarros);
     }
 
     private void prepararBD() {
@@ -111,14 +53,10 @@ public class Programa {
         }
     }
 
-
     private char gerarLetraAletoria() {
         final int NUM_LETRA_BASE = 97;
         final int QUANTIDADE_LETRAS = 25;
-
         int numero = GeradorNumAletorio.gerarInt(QUANTIDADE_LETRAS);
-
         return (char) (NUM_LETRA_BASE + numero);
     }
-
 }
